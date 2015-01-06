3a32d17c0ed81a95dd5011078e2d0a7607f6c036
Gene Liverman <gliverma@westga.edu>
2015-01-06 08:21:20 -0500

    Added template for /etc/security/pam_winbind.conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,29 @@
 #
 # Sample Usage:
 #
-class winbind {
+class winbind (
+  $pam_debug                 = 'no',
+  $pam_debug_state           = 'no',
+  $pam_cached_login          = 'yes',
+  $pam_krb5_auth             = 'no',
+  $pam_krb5_ccache_type      = '',
+  $pam_require_membership_of = ['',],
+  $pam_warn_pwd_expire       = '14',
+  $pam_silent                = 'no',
+  $pam_mkhomedir             = 'no',) {
+  # validate parameters
+  include stdlib
 
+  # strings
+  validate_string($pam_debug)
+  validate_string($pam_debug_state)
+  validate_string($pam_cached_login)
+  validate_string($pam_krb5_auth)
+  validate_string($pam_krb5_ccache_type)
+  validate_string($pam_warn_pwd_expire)
+  validate_string($pam_silent)
+  validate_string($pam_mkhomedir)
+
+  # arrays
+  validate_array($pam_require_membership_of)
 }
