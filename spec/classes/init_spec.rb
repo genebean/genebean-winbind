86cab938ae71d50501afcd9cc801ff87baa9c5e8
Gene Liverman <gliverma@westga.edu>
2015-09-10 21:00:08 -0400

    Added additional tests

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe 'winbind' do
+
+  describe 'on Red Hat' do
+    let :facts do
+      {
+        :kernel          => 'Linux',
+        :osfamily        => 'RedHat',
+        :operatingsystem => 'RedHat',
+        :fqdn            => 'SOMEHOST.ad.example.com'
+      }
+    end
+
+    # Check that all classes are present
+    it { should contain_class('stdlib')}
+    it { should contain_class('winbind::params')}
+    it { should contain_class('winbind::install')}
+    it { should contain_class('winbind::config')}
+    it { should contain_class('winbind::service')}
+
+    context 'with domain and login restrictions set' do
+      let :facts do
+        {
+          :kernel          => 'Linux',
+          :osfamily        => 'RedHat',
+          :operatingsystem => 'RedHat',
+          :fqdn            => 'SOMEHOST.ad.example.com'
+        }
+      end
+
+      let :pre_condition do
+        "class {'winbind':
+          pam_require_membership_of => ['sysadmins', 'iso-scans'],
+          smb_realm                 => 'AD.EXAMPLE.COM',
+          smb_workgroup             => 'AD',
+        }"
+      end
+
+      it 'should pass parameters to winbind::config' do
+        should contain_class('winbind::config').with(
+          'pam_require_membership_of' => '["sysadmins", "iso-scans"]',
+          'smb_realm'                 => 'AD.EXAMPLE.COM',
+          'smb_workgroup'             => 'AD',
+        )
+      end
+
+    end
+
+    context 'with package_ensure set to installed' do
+      let :facts do
+        {
+          :kernel          => 'Linux',
+          :osfamily        => 'RedHat',
+          :operatingsystem => 'RedHat',
+          :fqdn            => 'SOMEHOST.ad.example.com'
+        }
+      end
+
+      let :pre_condition do
+        "class {'winbind':
+          package_ensure => 'installed',
+        }"
+      end
+
+      it 'should pass parameters to winbind::install' do
+        should contain_class('winbind::install').with(
+          'package_ensure' => 'installed',
+        )
+      end
+    end
+
+    context 'with service management disabled' do
+      let :facts do
+        {
+          :kernel          => 'Linux',
+          :osfamily        => 'RedHat',
+          :operatingsystem => 'RedHat',
+          :fqdn            => 'SOMEHOST.ad.example.com'
+        }
+      end
+
+      let :pre_condition do
+        "class {'winbind':
+          manage_messagebus_service => false,
+          manage_oddjob_service     => false,
+        }"
+      end
+
+      it 'should pass parameters to winbind::service' do
+        should contain_class('winbind::service').with(
+          'manage_messagebus_service' => false,
+          'manage_oddjob_service'     => false,
+        )
+      end
+
+    end
+
+
+  end
+
+end
---
+++ spec/classes/install_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe 'winbind::install' do
+
+  context 'on Red Hat 5 with defaults' do
+    let :facts do
+      {
+          :kernel                    => 'Linux',
+          :osfamily                  => 'RedHat',
+          :operatingsystem           => 'RedHat',
+          :operatingsystemmajrelease => '5'
+      }
+    end
+
+    let :pre_condition do
+      "class {'winbind': }"
+    end
+
+    # Make sure package will be installed.
+    it { should contain_package('samba3x-winbind').with_ensure('latest') }
+    it { should have_package_resource_count(1) }
+
+  end
+
+  context 'on Red Hat 6 with defaults' do
+    let :facts do
+      {
+          :kernel                    => 'Linux',
+          :osfamily                  => 'RedHat',
+          :operatingsystem           => 'RedHat',
+          :operatingsystemmajrelease => '6'
+      }
+    end
+
+    let :pre_condition do
+      "class {'winbind': }"
+    end
+
+    # Make sure package will be installed.
+    it { should contain_package('samba-winbind-clients').with_ensure('latest') }
+    it { should contain_package('oddjob-mkhomedir').with_ensure('latest') }
+    it { should have_package_resource_count(2) }
+
+  end
+
+  context 'on Red Hat 7 with package_ensure set to installed' do
+    let :facts do
+      {
+          :kernel                    => 'Linux',
+          :osfamily                  => 'RedHat',
+          :operatingsystem           => 'RedHat',
+          :operatingsystemmajrelease => '7'
+      }
+    end
+
+    let :pre_condition do
+      "class {'winbind':
+        package_ensure => 'installed',
+      }"
+    end
+
+    # Make sure package will be installed.
+    it { should contain_package('samba-winbind-clients').with_ensure('installed') }
+    it { should contain_package('oddjob-mkhomedir').with_ensure('installed') }
+    it { should have_package_resource_count(2) }
+
+  end
+
+end
