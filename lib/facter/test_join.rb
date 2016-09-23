dae6c9c0c766132988999f2e8b78450b6258db6e
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-23 15:31:20 -0400

    #9: test_join.rb, fix syntax, add windows case

--- lib/facter/test_join.rb
+++ lib/facter/test_join.rb
@@ -3,7 +3,15 @@ Facter.add(:test_join) do
   setcode do
   
     # check if AD is joined
-    Facter::Core::Execution.exec("system 'net',  'ads',  'testjoin'`")
+    Facter::Core::Execution.exec("system 'net',  'ads',  'testjoin'")
+
+  end
+
+  confine :kernel => 'Windows'
+  setcode do
+
+    # check if AD is joined
+    Facter::Core::Execution.exec("system 'NET',  'USE',  '|', '%errorlevel%'")
 
   end
 end
