08d4f4c21bc3ec9fb1cf446386fbbd4f2334e768
System Administrator <jeff1evesque@yahoo.com>
2016-09-25 10:10:29 -0400

    #9: test_join.rb, add 'Windows' case, define docblock

--- lib/facter/test_join.rb
+++ lib/facter/test_join.rb
@@ -9,7 +9,7 @@ Facter.add(:test_join) do
   confine :kernel => 'Linux'
   setcode do
   
-    # check if AD is joined
+    # check if AD is joined: shell has been tested on unix systems
     Facter::Core::Execution.exec('if type -p net > /dev/null 2>&1; then if net ads testjoin; then echo "true"; else echo "false"; fi;  else echo "false"; fi')
 
   end
@@ -18,8 +18,11 @@ Facter.add(:test_join) do
   confine :kernel => 'Windows'
   setcode do
 
-    # check if AD is joined
-    Facter::Core::Execution.exec('NET USE')
+    # check if AD is joined: shell has been tested in Windows 7
+    #
+    # Note: the 'exit(1)' is used to exit the awk if match found
+    #
+    Facter::Core::Execution.exec('NET USE | awk \'{if (/There are no entries in the list/) {print "false"; exit(1)};}\' && printf "true"')
 
   end
 end
