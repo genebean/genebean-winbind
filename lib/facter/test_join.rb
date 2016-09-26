fe8622267a3cd2691d0828091d9981cc8c554757
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-26 11:50:47 -0400

    #9: test_join.rb, implement 'osfamily' cases

--- lib/facter/test_join.rb
+++ lib/facter/test_join.rb
@@ -1,47 +1,52 @@
 Facter.add(:test_join) do
-  ## linux case
-  #
-  #  Note: the following are additional resources:
-  #
-  #        - https://linux.die.net/man/8/net
-  #        - http://linuxcommand.org/lc3_man_pages/typeh.html
-  #
-  confine :kernel => 'Linux'
   setcode do
-    # local variables
-    cmd_cd = `cd ~`
-    cmd_check_install = `#{cmd_cd} && type -p net > /dev/null 2>&1; echo $?`
-    cmd_check_join = `net ads testjoin > /dev/null 2>&1; echo $?`
-  
-    # check if AD is joined
-    if cmd_check_install
+    osfamily = Facter.value('osfamily')
+
+    case osfamily
+    ## linux case
+    #
+    #  Note: the following are additional resources:
+    #
+    #        - https://linux.die.net/man/8/net
+    #        - http://linuxcommand.org/lc3_man_pages/typeh.html
+    #
+    when "Linux"
+      # local variables
+      cmd_cd = `cd ~`
+      cmd_check_install = `#{cmd_cd} && type -p net > /dev/null 2>&1; echo $?`
+      cmd_check_join = `net ads testjoin > /dev/null 2>&1; echo $?`
+
+      # check if AD is joined
+      if cmd_check_install
         if cmd_check_join
-            "true"
+          "true"
         else
-            "false"
+          "false"
         end
-    else
+      else
         "false"
-    end
-  end
+      end
+    ## windows case: tested on windows 7
+    #
+    #  Note: the following are addditional resources:
+    #
+    #        - https://technet.microsoft.com/en-us/library/bb490717.aspx
+    when "Windows"
+      # local variables
+      cmd_cd = `cd .`
+      cmd_check_join = `#{cmd_cd} && NET USE`
+      pattern = 'There are no entries in the list'
 
-  ## windows case
-  #
-  #  Note: the following are addditional resources:
-  #
-  #        - https://technet.microsoft.com/en-us/library/bb490717.aspx
-  confine :kernel => 'Windows'
-  setcode do
-    # local variables
-    pattern = 'There are no entries in the list'
-    cmd_cd = `cd .`
-    cmd_check_join = `#{cmd_cd} && NET USE`
-
-    # check if AD is joined
-    if cmd_check_join.include? pattern
+      # check if AD is joined
+      if cmd_check_join.include? pattern
         "false"
-    else
+      else
         "true"
+      end
+    ## else case: all other os families
+    else
+      "false"
     end
+
   end
 end
