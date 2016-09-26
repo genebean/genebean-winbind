480211c59cbbdfd5fadd224488374ad43e6b1aa5
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-26 12:24:13 -0400

    #9: test_winbind.rb, implement 'osfamily' cases

--- lib/facter/test_winbind.rb
+++ lib/facter/test_winbind.rb
@@ -1,34 +1,38 @@
 Facter.add(:test_winbind) do
-  ## linux case
-  #
-  #  Note: the following are additional resources:
-  #
-  #        - http://linuxcommand.org/man_pages/wbinfo1.html
-  #        - https://www.samba.org/samba/docs/man/manpages/wbinfo.1.html
-  #
-  confine :kernel => 'Linux'
   setcode do
-    # local variables
-    cmd_cd = `cd ~`
-    cmd_check_install = `#{cmd_cd} && type -p wbinfo > /dev/null 2>&1; echo $?`
-    cmd_ping_winbind = `wbinfo -p > /dev/null 2>&1; echo $?`
+    osfamily = Facter.value('osfamily')
 
-    # query winbind: check winbind is alive
-    if cmd_check_install
+    case osfamily
+    ## linux case
+    #
+    #  Note: the following are additional resources:
+    #
+    #        - http://linuxcommand.org/man_pages/wbinfo1.html
+    #        - https://www.samba.org/samba/docs/man/manpages/wbinfo.1.html
+    #
+    when "Linux"
+      # local variables
+      cmd_cd = `cd ~`
+      cmd_check_install = `#{cmd_cd} && type -p wbinfo > /dev/null 2>&1; echo $?`
+      cmd_ping_winbind = `wbinfo -p > /dev/null 2>&1; echo $?`
+
+      # query winbind: check winbind is alive
+      if cmd_check_install
         if cmd_ping_winbind
-            'true'
+          'true'
         else
-            'false'
-    else
+          'false'
+        end
+      else
         'false'
+      end
+    ## windows case: samba doesn't run on windows
+    when 'Windows'
+      Facter::Core::Execution.exec('echo "false"')
+    ## else case: all other os families
+    else
+      'false'
     end
-  end
-
-  ## windows case: samba doesn't run on windows
-  confine :kernel => 'Windows'
-  setcode do
-
-    Facter::Core::Execution.exec('echo "false"')
 
   end
 end
