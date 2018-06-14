da8bc9afb8c7903c1d24a1c5ed8a0350a8e8c02c
Serdar ARIKAN <serdar.arikan.ext@direct-energie.com>
2018-06-14 10:55:50 +0200

    KRB5 Add values in section realms

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,8 @@ class winbind::params {
   $krb5_dns_lookup_realm                = false
   $krb5_forwardable                     = true
   $krb5_kdc                             = 'FILE:/var/log/krb5kdc.log'
+  $krb5_realms_admin_server             = ''
+  $krb5_realms_kdc                      = ['',]
   $krb5_renew_lifetime                  = '7d'
   $krb5_ticket_lifetime                 = '24h'
   $manage_joindomain_script             = true
