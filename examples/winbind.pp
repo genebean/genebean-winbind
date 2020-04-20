cbf5b75ea0816db292ee3b6b58818f6a391d1fc9
Jake Spain <jakespain1@gmail.com>
2020-04-20 16:16:11 -0400

    Fixed issues with failing tests

--- examples/winbind.pp
+++ examples/winbind.pp
@@ -1,17 +1,16 @@
 # Sample profile for controlling domainJoin scripts
-class profiles::join_domain {
-  # Used on selected OS only
-  case $::kernel {
-    'Linux'   : {
-      include ::winbind
-    } # end Linux
+# Used on selected OS only
+case $::kernel {
+  'Linux'   : {
+    include ::winbind
+  } # end Linux
 
-    'Windows' : {
-    }
+  'Windows' : {
+  }
 
-    default : {
-      fail("${::operatingsystem} is not supported.")
-    }
+  default : {
+    fail("${::operatingsystem} is not supported.")
+  }
 
-  } # end case $::kernel
-}
+} # end case $::kernel
+
